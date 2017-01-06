9ed7cc679fc9cff8116948bf71cc8646d1403655
Jamey P Hanson <jamey@cloudera.com>
2017-01-06 09:56:49 -0500

    created pg_setup.py

---
+++ src/pg_setup.py
@@ -0,0 +1,12 @@
+'''
+Created on Jan 6, 2017
+
+@author: jamey
+Desc: create PostgreSQL tables for LAXStorm persistent storage and analysis
+'''
+
+def main():
+    print('pg_setup.py')
+
+if __name__ == '__main__':
+    main()
