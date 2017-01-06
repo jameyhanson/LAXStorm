4c35625af7fc26ad04798078d54cd452c893aeed
Jamey P Hanson <jamey@cloudera.com>
2017-01-06 10:18:54 -0500

    changed message

--- src/pg_setup.py
+++ src/pg_setup.py
@@ -6,7 +6,7 @@ Desc: create PostgreSQL tables for LAXStorm persistent storage and analysis
 '''
 
 def main():
-    print('pg_setup.py')
+    print('Lunch with Sandy')
 
 if __name__ == '__main__':
     main()
