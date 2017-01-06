789350ebc5a5a1c7b95fbf906be31152c18ba45a
Jamey P Hanson <jamey@cloudera.com>
2017-01-06 10:03:58 -0500

    added get_lax_ranks.py

---
+++ src/get_lax_ranks.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 6, 2017
+
+@author: jamey
+
+Desc: get lacrosse ranks from www.laxpower.com
+'''
+
+def main():
+    print('get_lax_ranks.py')
+    
+if __name__ == '__main__':
+    main()
