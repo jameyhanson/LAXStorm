f816bb767f543e27374b6d1153fa52f5e9335c34
Jamey P Hanson <jamey@cloudera.com>
2017-01-06 19:05:36 -0500

    added pg_normalize_hs.py

---
+++ src/pg_normalize_hs.py
@@ -0,0 +1,14 @@
+'''
+Created on Jan 6, 2017
+
+@author: jamey
+
+Desc: normalize the data in PostgreSQL so that high schools 
+      are in a consolidated table with a one-to-many relationship with the hs_rank.
+      This prevents duplicate high school geocode look ups
+'''
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
